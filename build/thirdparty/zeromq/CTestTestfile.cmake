ede0ba11bdbc4f1ee1ce0e8dee29a676cf3c03ee
huzelin <huzelin1008@163.com>
2018-12-03 12:43:45 +0800

    rm build

--- build/thirdparty/zeromq/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/app/HPPS/hpps/thirdparty/zeromq
-# Build directory: /home/app/HPPS/hpps/build/thirdparty/zeromq
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("zeromq-4.2.1")
