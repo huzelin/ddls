a267d62e6c3d29c88cf4c7218ba899d36de8a5c4
huzelin <huzelin1008@163.com>
2018-12-27 17:25:15 +0800

    add pull.sh

--- application/minst/data/pull.sh
+++ application/minst/data/pull.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+# training set images
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+# training set labels
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+# test set images
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+# test set labels
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+
