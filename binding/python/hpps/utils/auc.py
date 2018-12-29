d38284f2d31e764e194b60159e16c3f5efefdb6f
huzelin <huzelin1008@163.com>
2018-12-30 03:11:04 +0800

    add auc calc

--- binding/python/hpps/utils/auc.py
+++ binding/python/hpps/utils/auc.py
@@ -7,4 +7,23 @@ import types
 import logging
 
 def calc_auc(raw_arr):
+    """ Calc AUC
+    """
+    arr = sorted(raw_arr, key=lambda d:d[2])
+    
+    auc = 0.0
+    fp1, tp1, fp2, tp2 = 0.0, 0.0, 0.0, 0.0
+    for record in arr:
+        fp2 += record[0] 
+        tp2 += record[1]
+        auc += (fp2 - fp1) * (tp2 - tp1)
+        fp1, tp1 = fp2, tp2
 
+    threshold = len(arr) - 1e-3
+    if tp2 > threshold or fp2 > threshold:
+        return -0.5
+
+    if tp2 * fp2 > 0.0:
+        return (1.0 - auc / (2.0 * tp2 * fp2))
+    else:
+        return None
