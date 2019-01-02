9c619781fe9400dbe9fd56719e24bf06af63c4d0
huzelin <huzelin1008@163.com>
2019-01-02 20:27:54 +0800

    update install.sh

--- binding/python/install.sh
+++ binding/python/install.sh
@@ -1,3 +1,3 @@
 /usr/local/anaconda2/bin/python setup.py install
 python setup.py install
-rm -rf build dist hpps/*.pyc tracker/*.pyc hpps.egg-info 
+rm -rf build dist ddls/*.pyc tracker/*.pyc ddls.egg-info 
