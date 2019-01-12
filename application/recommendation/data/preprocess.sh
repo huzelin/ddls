468e2a7d83f17e796a283735858dd0ccb97e1915
huzelin <huzelin1008@163.com>
2019-01-12 18:01:36 +0800

    update script for ads data

--- application/recommendation/data/preprocess.sh
+++ application/recommendation/data/preprocess.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cat sample_skeleton_train.csv | sort -t "," -k 4 > sort_sample_skeleton_train.csv
 cat common_features_train.csv | sort -t "," -k 1 > sort_common_features_train.csv
+
+cat sample_skeleton_test.csv | sort -t "," -k 4 > sort_sample_skeleton_test.csv
+cat common_features_test.csv | sort -t "," -k 1 > sort_common_features_test.csv
--- application/recommendation/data/run.sh
+++ application/recommendation/data/run.sh
@@ -1,2 +1,11 @@
 #/bin/sh
+# NOTE: You should unzip the sample
+sh preprocess.sh
+
+mkdir -p samples
 python gen_dist_sample.py sort_common_features_train.csv sort_sample_skeleton_train.csv 
+mv samples trains
+
+mkdir -p samples
+python gen_dist_sample.py sort_common_features_test.csv sort_sample_skeleton_test.csv 
+mv samples tests
